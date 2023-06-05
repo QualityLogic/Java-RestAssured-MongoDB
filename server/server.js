699be6424885eeaa166faca5a4e64e1b2c8fd5e6
qlibcrist <bcrist@qualitylogic.com>
2023-06-05 13:17:07 -0600

    Adds db connection to mongodb

--- server/server.js
+++ server/server.js
@@ -2,11 +2,23 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+require('dotenv').config()
+require('./config/database').connect()
+const express = require('express')
 
-const PORT = 3000
+const PORT = process.env.PORT
 
 server.use(middlewares)
+server.use(express.json())
+
+// Auth
+server.use((req, res, next) => {
+
+})
+
 server.use(router)
 server.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`)
 })
+
+module.exports = server
